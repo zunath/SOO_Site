e9a869d82f948c61494ded879c358f0b1c3cf5e1
zunath <coolty3001@yahoo.com>
2015-09-28 21:00:32 -0500

    Config files

---
+++ SOOSite/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+using Autofac;
+using Autofac.Integration.SignalR;
+using Microsoft.AspNet.SignalR;
+
+namespace SOOSite
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            ContainerBuilder builder = new ContainerBuilder();
+            builder.RegisterHubs(Assembly.GetExecutingAssembly());
+            GlobalHost.DependencyResolver = new AutofacDependencyResolver(builder.Build());
+        }
+    }
+}
