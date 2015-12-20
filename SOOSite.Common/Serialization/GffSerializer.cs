e971cc3f212501bddeff8137dfa00f09306b9029
zunath <coolty3001@yahoo.com>
2015-12-19 20:00:29 -0600

    GffSerializer

---
+++ SOOSite.Common/Serialization/GffSerializer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using SOOSite.Common.GffRawStructures;
+using SOOSite.Common.NWObjects;
+
+namespace SOOSite.Common.Serialization
+{
+    public static class GffSerializer
+    {
+        public static T Deserialize<T>(string filePath)
+            where T : INWObject
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Gff));
+            string data = File.ReadAllText(filePath);
+
+            using (TextReader reader = new StringReader(data))
+            {
+                Gff gff = (Gff)serializer.Deserialize(reader);
+                T instance = (T)Activator.CreateInstance(typeof(T));
+
+                return (T)instance.ToMappedObject(gff);
+            }
+        }
+
+    }
+}
